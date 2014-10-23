4326a205060ee9779c1d270dc047efb3dc99b412
Max Snyder <maxsnyder@g.harvard.edu>
2014-10-22 17:06:46 -1000

    Changed font and background color

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -4,9 +4,10 @@
 
 		<title>@yield('title', 'P3')</title>
 		<style>
-			h1{text-align:center;}
-			h2{text-align:center; font-size:80%}
-			body{text-align:center;}
+
+			h1{text-align:center;background-color: rgb(145,198,164); font-family: Arial, Verdana, Sans-serif; }
+			h2{text-align:center; font-size:80%; background-color: rgb(145,198,164);font-family: Arial, Verdana, Sans-serif;}
+			body{text-align:center;background-color: rgb(145,198,164);font-family: Arial, Verdana, Sans-serif;}
 		</style>
 		@yield('head')
 
